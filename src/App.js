65010a33a6d37d8e84c29a08f206862e0cd9c9ef
Guglielmo Turco <guglio@users.noreply.github.com>
2021-09-23 14:46:51 -0700

    Add react router hash

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import './App.css';
-import Inventory from './components/Inventory';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-function App() {
+import Inventory from './components/Inventory'
+
+function App () {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Inventory />
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Inventory />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
